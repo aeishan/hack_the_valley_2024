8ddbc088e3fe88767f17af317d042efb633cf5b5
KKAPOOR30 <99267123+KKAPOOR30@users.noreply.github.com>
2024-10-05 00:47:23 -0400

    Update Backend.py

--- Backend.py
+++ Backend.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def register():
-    return render_template()
+    return jsonify({"Hell0" : "World"})
 
 
 if __name__ == '__main__':
