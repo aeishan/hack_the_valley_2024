4394598454e7a1f6e366e0ba47b8f18fae896d94
aeishan <ashrafeishan@gmail.com>
2024-10-06 01:19:13 -0400

    feat: camera commits

--- components/Button.js
+++ components/Button.js
@@ -1,25 +1,30 @@
+//component Button for icons to use in the camera
+
 import * as React from 'react';
-import { Stylesheet, Text, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity} from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 export default Button = ({icon, size, color, style, onPress }) => {
 
     return (
-        <TouchableOpacity style={[Stylesheet.button, size]} onPress={onPress}>
-            <MaterialIcon 
+        <TouchableOpacity 
+            style={[styles.button, style]}
+            onPress={onPress}
+        >
+            <MaterialIcons 
                 name = {icon}
-                size = {size? size : 28} // min size of item is 28
-                color = {color? color: '#f1f1f1'}
+                size={size? size : 28} //if size is passed in props we use it else we use 28
+                color={color ? color : '#f1f1f1'}
             />
         </TouchableOpacity>
     )
 
 }
 
-const styles = Stylesheet.create({
-    button: {
-        height: 40,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+const styles = StyleSheet.create({
+  button : {
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 })
